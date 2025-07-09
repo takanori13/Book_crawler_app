07b87c4926899f625e20c24ca07e7153e8e4e022
takanori13 <iwatavix@gmail.com>
2025-07-09 00:10:49 -0800

    Update books_spider.py

--- scrapy_project/scrapy_project/spiders/books_spider.py
+++ scrapy_project/scrapy_project/spiders/books_spider.py
@@ -2,16 +2,19 @@ import scrapy
 
 class BooksSpider(scrapy.Spider):
     name = "books"
-    start_urls = ['https://books.toscrape.com/']
+    start_urls = ['http://books.toscrape.com/catalogue/page-1.html']
 
     def parse(self, response):
-        for book in response.css('article.product_pod'):
+        for book in response.css("article.product_pod"):
             yield {
-                'title': book.css('h3 a::attr(title)').get(),
-                'price': book.css('.price_color::text').get(),
-                'availability': book.css('.availability::text').re_first('\S+'),
+                "title": book.css("h3 a::attr(title)").get(),
+                "price": book.css(".price_color::text").get(),
+                "availability": book.css(".availability::text").re_first("\w+"),
+                "rating": book.css("p.star-rating::attr(class)").re_first("star-rating (\w+)"),
+                "link": response.urljoin(book.css("h3 a::attr(href)").get())
             }
 
-        next_page = response.css('li.next a::attr(href)').get()
+        # <- ini penting: harus di luar loop
+        next_page = response.css("li.next a::attr(href)").get()
         if next_page:
             yield response.follow(next_page, self.parse)
